4fa3e0428ca3ccf6ab6cc55ca0d3cf92537f51ea
jhamski <hamski.2@gmail.com>
2015-05-10 22:28:24 -0400

    First queries related to R stats

--- twitter_user_graph_data.R
+++ twitter_user_graph_data.R
@@ -1,6 +1,13 @@
+# IS607 Data Acquisition and Management | CUNY MS Data Analytics | James Hamski
+
+# Project Goal: create a social graph of the R Stats community on Twitter
+# Can we find different domain groups (i.e. biostats, finance)?
+# Can we determine key influencer based on retweets?
 
 library(twitteR)
+library(dplyr)
 
+# Keys stored in a txt file and kept out of Github repo
 key <- scan("api_key.txt", what="charsacter")
 
 consumer_key <- key[1]
@@ -12,4 +19,19 @@ setup_twitter_oauth(consumer_key,
                     access_token,
                     access_secret)
 
-r_stats <- searchTwitter("#Rstats", n=1500)
+#Who uses the #RStats hashtag the most?
+r.stats.search <- searchTwitter("#Rstats", n=1000)
+
+r.stats.search%>%
+  twListToDF()%>%
+  group_by(screenName) %>%
+  summarise(count = n()) %>%
+  arrange(desc(count)) -> r.stats
+  
+#Start with Hadley Wickham, well known R Stats package developer/guru
+hw <- getUser("hadleywickham") 
+hw.friends <-friendships("hadleywickham", n=1000)
+hw.favorites <- favorites("hadleywickham", n=100)
+hw.retweets <- retweets("hadleywickham", n=100)
+
+twListToDF(twList)
