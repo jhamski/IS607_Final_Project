e4dd2a47b6d1a61f2189743f0767640c0d88d48d
jhamski <hamski.2@gmail.com>
2015-05-19 22:54:54 -0400

    Another script for cleaning

--- twitter_graph_APIpull.R
+++ twitter_graph_APIpull.R
@@ -12,6 +12,14 @@ library(dplyr)
 # Keys stored in a txt file and kept out of Github repo
 key <- scan("api_key.txt", what="charsacter")
 
+consumer_key <- key[1]
+consumer_secret <- key[2]
+access_token <- key[3]
+access_secret <- key[4]
+setup_twitter_oauth(consumer_key,
+                    consumer_secret,
+                    access_token,
+                    access_secret)
 
 #Who uses the #RStats hashtag the most?
 r.stats.search <- searchTwitter("#Rstats", n=1000)
--- twitter_graph_NeoGraph.R
+++ twitter_graph_NeoGraph.R
@@ -1,5 +1,5 @@
 # IS607 Data Acquisition and Management | CUNY MS Data Analytics | James Hamski
-# Script 2 - load data and create graph DB
+# Script 3 - load data and create graph DB
 
 # Project Goal: create a social graph of the R Stats community on Twitter
 # Can we find different domain groups (i.e. biostats, finance)?
@@ -8,8 +8,8 @@
 # Primary reference: "Demo of RNeo4j Part 1: Building a Database" by Nicole White
 # http://nicolewhite.github.io/r/2014/05/30/demo-of-rneo4j-part1.html
 
-install.packages("devtools")
-devtools::install_github("nicolewhite/RNeo4j")
+#install.packages("devtools")
+#devtools::install_github("nicolewhite/RNeo4j")
 library(RNeo4j)
 
 
@@ -19,7 +19,9 @@ sean <- c("nate", "matt", "sara", "pete", "cate", "laura")
 laura <- c("maura", "sean", "greg", "matt", "sara")
 albert <- c("nate", "sara", "roger", "maura")
 
-
-
-
 #Create graph for real dataset 
+#Connect to Neo4j after starting the server at the command line. Note that you may need to change the password 
+#in the Neo4j console before moving on. 
+
+graph = startGraph("http://localhost:7474/db/data/", username = "neo4j", password = "msda16")
+clear(graph)
