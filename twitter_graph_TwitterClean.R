a65f140acab55ba11fdf0fb8bfcfdfc3e6d285a1
jhamski <hamski.2@gmail.com>
2015-05-23 23:05:23 -0400

    deleted depreciated script

--- twitter_graph_TwitterClean.R
+++
@@ -1,7 +0,0 @@
-# IS607 Data Acquisition and Management | CUNY MS Data Analytics | James Hamski
-# Script 2 - clean data from the Twitter API
-
-
-load("followers_dataset.Rda")
-
-# This script is depreciated...
